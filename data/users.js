e9230e61aafde579edf1be49c28cd6f69a318792
nkmiller7 <nkmiller78787@gmail.com>
2025-10-21 10:43:18 -0400

    Added the ability to create messages on forums and create games and added the seed.js file to seed database.

--- data/users.js
+++ data/users.js
@@ -1,4 +1,4 @@
-import { users } from "../config/mongoCollections";
+import { users } from "../config/mongoCollections.js";
 import validation from './validation.js';
 
 const exportedMethods = {
@@ -14,7 +14,27 @@ const exportedMethods = {
     const user = await userCollection.findOne({ _id: new ObjectId(id) });
     if (!user) throw 'Error: User not found';
     return user;
-  }
+  },
+
+  async addUser(firstName, lastName, email, password, isAnnonymous = false) {
+    firstName = validation.checkString(firstName, 'First name');
+    lastName = validation.checkString(lastName, 'Last name');
+    email = validation.checkEmail(email, "Email");
+    password = validation.checkString(password, 'Password');
+
+    let newUser = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      isAnnonymous: isAnnonymous
+    };
+    const userCollection = await users();
+    const newInsertInformation = await userCollection.insertOne(newUser);
+    if (!newInsertInformation.insertedId) throw 'Insert failed!';
+    return await this.getUserById(newInsertInformation.insertedId.toString());
+  },
+
 };
 
 export default exportedMethods;
