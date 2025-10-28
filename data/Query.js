92451263d2deffe89bbbc3c7d45cafdab3c8eedc
marc-sulsenti <marcdantesulsenti@gmail.com>
2025-10-28 18:44:10 -0400

    Reformatted the Website Logo. Added the website's logo into the header and fixed sizing issues. Added the website footer.

---
+++ data/Query.js
@@ -0,0 +1,38 @@
+/*
+
+More advanced queries for filtering specific
+
+Things we can possibly filter on
+- Court Type (Basketball, Tennis, Both)
+- Available Dates/Times
+- Location (Something that is an additional feature)
+- Sort by highest rated / most popular
+- Sort by active gaves
+
+
+Based on current JSON Data
+- Accessibility status
+- Tennis Courts
+    - Number of Tennis Courts
+    - Court Type (Clay, Grass, Hard)
+    - Offering Lessons
+- Basketball Courts
+    - Number of Basketball Courts
+    - Rec Center vs Park Courts
+    - Indoor/Outdoor
+*/
+
+//Seed the database, then we need to see how the actual data looks
+// From that we know how to query, atleast something like the locations page
+
+
+export const searchLocations = async (collection,filters) => {
+    // where collection is some collection and filters is an object with possible filter keys
+    if (!collection || !Array.isArray(collection)) {
+        throw new Error('Collection provided is null or not an array');
+    }
+
+    
+
+return filteredResults
+}
