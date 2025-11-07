53e8c7e17693dffeb70f1cef2ed67ea987027a15
jake <jake@jake-laptop>
2025-11-07 10:44:00 -0500

    Added about us.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import userRoutes from './users.js';
 import locationRoutes from './locations.js';
 import reviewRoutes from './reviews.js';
 import forumRoutes from './forums.js';
+import aboutRoutes from './about.js';
 
 
 const constructorMethod = (app) => {
@@ -9,6 +10,7 @@ const constructorMethod = (app) => {
   app.use('/locations', locationRoutes);
   app.use('/reviews', reviewRoutes);
   app.use('/forums', forumRoutes);
+  app.use('/about', aboutRoutes);
 
   app.use('{*splat}', (req, res) => {
     return res.status(404).json({error: 'Not found'});
