f1d2c720be64f66911ea83bc130c23626f10653a
jake <jacobpellini03@gmail.com>
2025-11-09 15:23:47 -0500

    Added barebones landing page.

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
-import userRoutes from './users.js';
-import locationRoutes from './locations.js';
-import reviewRoutes from './reviews.js';
-import forumRoutes from './forums.js';
-import aboutRoutes from './about.js';
-
+import userRoutes from "./users.js";
+import locationRoutes from "./locations.js";
+import reviewRoutes from "./reviews.js";
+import forumRoutes from "./forums.js";
+import aboutRoutes from "./about.js";
 
 const constructorMethod = (app) => {
-  app.use('/users', userRoutes);
-  app.use('/locations', locationRoutes);
-  app.use('/reviews', reviewRoutes);
-  app.use('/forums', forumRoutes);
-  app.use('/about', aboutRoutes);
+  app.use("/users", userRoutes);
+  app.use("/locations", locationRoutes);
+  app.use("/reviews", reviewRoutes);
+  app.use("/forums", forumRoutes);
+  app.use("/about", aboutRoutes);
+  app.get("/", (req, res) => {
+    return res.render("landing/index", { layout: "landing" });
+  });
 
-  app.use('{*splat}', (req, res) => {
-    return res.status(404).render('errors/404');
+  app.use("{*splat}", (req, res) => {
+    return res.status(404).render("errors/404");
   });
 };
 
