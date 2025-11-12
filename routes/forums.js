c57b6cce23ce593981e6e430fec05303d6a6cdf4
jsoiferm_stevens <jsoiferm@stevens.edu>
2025-11-12 11:41:20 -0500

    Added Post route for creating a forum message

--- routes/forums.js
+++ routes/forums.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import { forumData } from '../data/index.js';
 import { userData } from '../data/index.js';
+import { users } from "../config/mongoCollections.js";
 import validation from '../data/validation.js';
 
 const router = Router();
@@ -17,7 +18,7 @@ router.get('/:id', async (req, res) => {
 
 router.get('/locations/:id', async (req, res) => {
   try {
-    const id = validation.checkId(req.params.id);
+    const id = validation.checkId(req.params.id, "Location ID");
     const forumOld = await forumData.getForumMessagesByLocationId(id);
     forum = []
     for(let c of forumOld){
@@ -44,6 +45,22 @@ router.get('/locations/:id', async (req, res) => {
     res.status(404).json({ error: e.toString() });
   }
 });
+router.post('/locations/:id', async(req, res) => {
+  try{
+    const locationId = validation.checkId(req.params.id, "Location ID");
+    const userCollection = await users();
+    const user = await userCollection.findOne({email: req.session.user.email});
+    if(!user){
+      throw 'Error: User not found';
+    }
+    const userId = user._id.toString();
+    const content = req.body;
+    await forumData.createMessage(locationId, userId, content);
+
+  } catch(e){
+    return res.status(500).json({ error: e.message });
+  }
+})
 
 
 export default router;
