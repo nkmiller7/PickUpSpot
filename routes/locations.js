9e1ef9a7099d36f6e08d992bc94757d3c56dc3c4
jsoiferm_stevens <jsoiferm@stevens.edu>
2025-11-24 17:34:00 -0500

    Ability to delete comment and other small changes

--- routes/locations.js
+++ routes/locations.js
@@ -6,6 +6,10 @@ import { reviewData } from '../data/index.js';
 import { searchLocation } from '../data/Query.js'; 
 import { users } from "../config/mongoCollections.js"; 
 import validation from '../data/validation.js';
+import { reviews } from  "../config/mongoCollections.js"; 
+import { ObjectId } from "mongodb";
+import { forums } from "../config/mongoCollections.js";
+
 
 const router = Router();
 
@@ -73,6 +77,12 @@ router
   .route('/:id')
   .get(async (req, res) => {
   try {
+    const userCollection = await users();
+    const user = await userCollection.findOne({email: req.session.user.email});
+    if(!user){
+      throw 'Error: User not found'
+    }
+    const userId = user._id.toString();
     const id = validation.checkId(req.params.id);
     const location = await locationData.getLocationById(id);
     const forumOld = await forumData.getForumMessagesByLocationId(id);
@@ -82,27 +92,31 @@ router
       if(user.isAnonymous === false){
       forum.push(
           {
+            messageId: c._id,
             userName: user.firstName+" "+user.lastName,
             content: c.content,
-            createdAt: c.createdAt
+            createdAt: c.createdAt,
+            byUser: userId === c.userId.toString()
           }
         )
       }else{
         forum.push(
           {
+            messageId: c._id,
             content: c.content,
-            createdAt: c.createdAt
+            createdAt: c.createdAt,
+            byUser: userId === c.userId.toString()
           }
         )
       }
     }
     const reviewsOld= await reviewData.getReviewsByLocationId(id);
-    const reviews = [];
+    const reviewList = [];
     const ratings = [];
     for(let r of reviewsOld){
       let user = await userData.getUserById(r.userId.toString());
       if(user.isAnonymous === false){
-        reviews.push(
+        reviewList.push(
           {
             userName: user.firstName+" "+user.lastName,
             rating: r.rating,
@@ -112,7 +126,7 @@ router
           }
         )
       }else{
-        reviews.push(
+        reviewList.push(
           {
             rating: r.rating,
             comment: r.comment,
@@ -127,21 +141,34 @@ router
     for(let rating of ratings){
       ratings_sum+=rating;
     }
-    const averageRating = ratings_sum/ratings.length;
-    const userCollection = await users();
-    const user = await userCollection.findOne({email: req.session.user.email});
+    let averageRating = (ratings_sum/ratings.length).toFixed(1);
+    averageRating = parseFloat(averageRating);
     if(!user){
       throw 'Error: User not found';
     }
     const hasVisited = (user.parksAttended.includes(id));
+    const reviewCollection= await reviews();
+    const oldReview= await reviewCollection.find({$and: [{userId: new ObjectId(userId)}, {locationId: new ObjectId(id)}]}).toArray();
+    let alreadyReviewed;
+    if(oldReview.length === 0){
+      alreadyReviewed= false;
+    }else{
+      alreadyReviewed= true;
+    }
     res.render('locations/single', { location: location, forum: forum, 
-      reviews: reviews, averageRating: averageRating, singleLocation: true, 
-      user: req.session.user, locationId: id, hasVisited: hasVisited});
+      reviews: reviewList, averageRating: averageRating, singleLocation: true, 
+      user: req.session.user, locationId: id, hasVisited: hasVisited, alreadyReviewed: alreadyReviewed});
   } catch (e) {
     res.status(404).json({ error: e.toString() });
   }
 })
   .post(async(req, res) => {
+    const userCollection = await users();
+    const user = await userCollection.findOne({email: req.session.user.email});
+    if(!user){
+      throw 'Error: User not found';
+    }
+    const userId = user._id.toString();
     const id = validation.checkId(req.params.id);
     const location = await locationData.getLocationById(id);
     const forumOld = await forumData.getForumMessagesByLocationId(id);
@@ -151,27 +178,31 @@ router
       if(user.isAnonymous === false){
       forum.push(
           {
+            messageId: c._id,
             userName: user.firstName+" "+user.lastName,
             content: c.content,
-            createdAt: c.createdAt
+            createdAt: c.createdAt,
+            byUser: userId === c.userId.toString()
           }
         )
       }else{
         forum.push(
           {
+            messageId: c._id,
             content: c.content,
-            createdAt: c.createdAt
+            createdAt: c.createdAt,
+            byUser: userId === c.userId.toString()
           }
         )
       }
     }
     const reviewsOld= await reviewData.getReviewsByLocationId(id);
-    const reviews = [];
+    const reviewList = [];
     const ratings = [];
     for(let r of reviewsOld){
       let user = await userData.getUserById(r.userId.toString());
       if(user.isAnonymous === false){
-        reviews.push(
+        reviewList.push(
           {
             userName: user.firstName+" "+user.lastName,
             rating: r.rating,
@@ -181,7 +212,7 @@ router
           }
         )
       }else{
-        reviews.push(
+        reviewList.push(
           {
             rating: r.rating,
             comment: r.comment,
@@ -196,18 +227,21 @@ router
     for(let rating of ratings){
       ratings_sum+=rating;
     }
-    const averageRating = ratings_sum/ratings.length;
+    let averageRating = (ratings_sum/ratings.length).toFixed(1);
+    averageRating = parseFloat(averageRating);
     let errors = [];
     let hasVisited= false;
+    let alreadyReviewed;
     try{
       const locationId = validation.checkId(req.params.id, "Location ID");
-      const userCollection = await users();
-      const user = await userCollection.findOne({email: req.session.user.email});
-      if(!user){
-        throw 'Error: User not found';
-      }
-      const userId = user._id.toString();
       hasVisited=user.parksAttended.includes(id);
+      const reviewCollection= await reviews();
+      const oldReview= await reviewCollection.find({$and: [{userId: new ObjectId(userId)}, {locationId: new ObjectId(id)}]}).toArray();
+      if(oldReview.length === 0){
+        alreadyReviewed= false;
+      }else{
+        alreadyReviewed= true;
+      }
       let content = req.body.content;
       content= validation.checkString(content, "Comment Content");
       if(content.length < 5 || content.length > 500){
@@ -224,12 +258,13 @@ router
         content: req.body.content,
         location: location, 
         forum: forum, 
-        reviews: reviews, 
+        reviews: reviewList, 
         averageRating: averageRating, 
         singleLocation: true, 
         user: req.session.user, 
         locationId: id,
-        hasVisited: hasVisited
+        hasVisited: hasVisited,
+        alreadyReviewed: alreadyReviewed
       });
       return;
     }
@@ -239,6 +274,121 @@ router
       res.status(500).json({error: e});
     }
 })
+  .delete(async(req, res) => {
+    const userCollection = await users();
+      const user = await userCollection.findOne({email: req.session.user.email});
+      if(!user){
+        throw 'Error: User not found';
+      }
+    const userId = user._id.toString();
+    const id = validation.checkId(req.params.id);
+    const location = await locationData.getLocationById(id);
+    const forumOld = await forumData.getForumMessagesByLocationId(id);
+    const forum = [];
+    for(let c of forumOld.reverse()){
+      let user = await userData.getUserById(c.userId.toString());
+      if(user.isAnonymous === false){
+      forum.push(
+          {
+            messageId: c._id,
+            userName: user.firstName+" "+user.lastName,
+            content: c.content,
+            createdAt: c.createdAt,
+            byUser: userId === c.userId.toString()
+          }
+        )
+      }else{
+        forum.push(
+          {
+            messageId: c._id,
+            content: c.content,
+            createdAt: c.createdAt,
+            byUser: userId === c.userId.toString()
+          }
+        )
+      }
+    }
+    const reviewsOld= await reviewData.getReviewsByLocationId(id);
+    const reviewList = [];
+    const ratings = [];
+    for(let r of reviewsOld){
+      let user = await userData.getUserById(r.userId.toString());
+      if(user.isAnonymous === false){
+        reviewList.push(
+          {
+            userName: user.firstName+" "+user.lastName,
+            rating: r.rating,
+            comment: r.comment,
+            createdAt: r.createdAt,
+            updatedAt: r.updatedAt
+          }
+        )
+      }else{
+        reviewList.push(
+          {
+            rating: r.rating,
+            comment: r.comment,
+            createdAt: r.createdAt,
+            updatedAt: r.updatedAt
+          }
+        )
+      }
+      ratings.push(r.rating);
+    }
+    let ratings_sum = 0;
+    for(let rating of ratings){
+      ratings_sum+=rating;
+    }
+    let averageRating = (ratings_sum/ratings.length).toFixed(1);
+    averageRating = parseFloat(averageRating);
+    let errors = [];
+    let hasVisited= false;
+    let alreadyReviewed;
+    try{
+      const locationId = validation.checkId(req.params.id, "Location ID");
+      hasVisited=user.parksAttended.includes(id);
+      const reviewCollection= await reviews();
+      const oldReview= await reviewCollection.find({$and: [{userId: new ObjectId(userId)}, {locationId: new ObjectId(id)}]}).toArray();
+      if(oldReview.length === 0){
+        alreadyReviewed= false;
+      }else{
+        alreadyReviewed= true;
+      }
+      let messageId= req.body.messageId;
+      messageId = validation.checkId(messageId, "Message ID");
+      const messageCollection = await forums();
+      const message= await messageCollection.findOne({_id: new ObjectId(messageId)});
+      if(!message){
+        throw 'Error: message not found'
+      }
+      if (message.userId.toString() !== userId) throw 'Error: You can only delete your own messages';
+      await forumData.deleteMessage(messageId, userId);
+    }catch(e){
+      errors.push(e);
+    }
+    if(errors.length > 0){
+      res.render('locations/single', {
+        errors: errors,
+        hasErrors: true,
+        content: req.body.content,
+        location: location, 
+        forum: forum, 
+        reviews: reviewList, 
+        averageRating: averageRating, 
+        singleLocation: true, 
+        user: req.session.user, 
+        locationId: id,
+        hasVisited: hasVisited,
+        alreadyReviewed: alreadyReviewed,
+      });
+      return;
+    }
+    try{
+      res.redirect(`/locations/${req.params.id}`);
+    }catch(e){
+      res.status(500).json({error: e});
+    }
+  })
   
 export default router;
 
